227a68f1cc203673d9fabe1381dbfc38c7875811
P Hawley <ph1499@nyu.edu>
2024-11-24 10:52:20 -0500

    plot whole trajectory

--- output.py
+++ output.py
@@ -13,7 +13,8 @@ def print_sample(sample):
     for t in range(steps):
         print_image(position_channel[t], walls_channel[t])
 
-    plot_image(position_channel[1], walls_channel[1])
+    #plot_image(position_channel[1], walls_channel[1])
+    plot_trajectory(position_channel, walls_channel)
 
 def print_image(position, walls):
 
@@ -46,4 +47,29 @@ def plot_image(position, walls):
 
         fig.tight_layout()
         fig.savefig('traject.png')
+
+def plot_trajectory(position_channel, walls_channel):
+    """
+    Plot the entire trajectory of position and walls.
+    """
+    steps = position_channel.shape[0]
+
+    fig, axes = plt.subplots(steps, 2, figsize=(12, 6 * steps))
+
+    if steps == 1:
+        axes = axes[np.newaxis, :]  # Make it 2D for single timestep
+
+    for t in range(steps):
+        ax_pos = axes[t, 0]
+        im_pos = ax_pos.imshow(position_channel[t], cmap="viridis")
+        ax_pos.set_title(f"Position - Timestep {t}")
+        fig.colorbar(im_pos, ax=ax_pos)
+
+        ax_wall = axes[t, 1]
+        im_wall = ax_wall.imshow(walls_channel[t], cmap="gray")
+        ax_wall.set_title(f"Walls/Doors - Timestep {t}")
+        fig.colorbar(im_wall, ax=ax_wall)
+
+    fig.tight_layout()
+    fig.savefig('traject.png')
 
