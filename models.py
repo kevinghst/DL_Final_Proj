3e384144426248e8225bacbae2479265e12b70e1
Zhiyu Guo <zg915@nyu.edu>
2024-12-03 15:48:34 -0500

    debug

--- models.py
+++ models.py
@@ -91,8 +91,9 @@ class LowEnergyTwoModel(nn.Module):
         bs, action_length, action_dim = actions.shape # (bs, 16, 2)
 
         encoded_states = self.encoder(states[:,:1])
-        encoded_target_states = self.target_encoder(states)
-        encoded_target_states = encoded_target_states[:,:-1]
+        encoded_target_states = None
+        if not self.training:
+            encoded_target_states = self.target_encoder(states[:, :-1])
 
 
         predicted_states = []
